89c27635164cc807321b6fcc90b66f9552f54c9d
kalpeshshirsath11 <shirsathkalpesh2005@gmail.com>
2024-11-07 11:54:56 +0530

    added

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,19 @@
-const asyncHandler = (requestHandler)=>{
-    (req,res,next) =>{
-        Promise.resolve().catch((err)=>{
-            next(err)
-        })
-    }
-}
+// asyncHandler is a higher-order function that takes a request handler function as an argument
+const asyncHandler = (requestHandler) => {
+    // Returns a new function that Express will use as the middleware
+    return (req, res, next) => {
+        // Calls the request handler function and resolves any returned promise
+        // If the promise is rejected (an error occurs), catch the error
+        // and pass it to the 'next' function to handle in Express
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+            next(err); // Passes any error to the next middleware (error handler)
+        });
+    };
+};
 
-export {asyncHandler}
+// Exports the asyncHandler function for use in other files
+export { asyncHandler };
+
 
 // const asyncHandler =(func)=>{async(req,res,next)=>{
 //     try{
